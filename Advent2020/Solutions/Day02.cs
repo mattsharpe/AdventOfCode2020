31bfa03545d658826cea330cc02586fb7522315a
Matthew Sharpe <matthew.sharpe@sword-grc.com>
2020-12-02 10:05:32 +0000

    Day 2 - Validating passwords with a Regular Expression

---
+++ Advent2020/Solutions/Day02.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Advent2020.Solutions
+{
+    class Day02
+    {
+        public bool IsValid(string line)
+        {
+            var regex = new Regex(@"(\d*)-(\d*) ([a-z]): (\w*)");
+            var match = regex.Match(line);
+            var min = Convert.ToInt32(match.Groups[1].Value);
+            var max = Convert.ToInt32(match.Groups[2].Value);
+            var character = match.Groups[3].Value[0];
+            var password = match.Groups[4].Value;
+
+            var instances = password.Count(x => x.Equals(character));
+            return instances >= min && instances <= max;
+        }
+
+        public int CountValidPasswords(string[] sample)
+        {
+            return sample.Count(IsValid);
+        }
+    }
+}
---
+++ Advent2020/Tests/Day02Tests.cs
@@ -0,0 +1,66 @@
+﻿using System.IO;
+using Advent2020.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2020.Tests
+{
+    [TestClass]
+    public class Day02Tests
+    {
+        private Day02 _day02;
+        private string[] _sample;
+        private string[] _input;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _day02 = new Day02();
+            _sample = new []
+            {
+                "1-3 a: abcde",
+                "1-3 b: cdefg",
+                "2-9 c: ccccccccc",
+                "12-15 h: hhhhhrhcxhhhhhhhhb"
+            };
+            _input = File.ReadAllLines("Input/Day02.txt");
+        }
+
+        [TestMethod]
+        public void Sample()
+        {
+            var result = _day02.CountValidPasswords(_sample);
+            Assert.AreEqual(3, result);
+        }
+
+        [DataTestMethod]
+        [DataRow("1-3 a: abcde", true)]
+        [DataRow("1-3 b: cdefg", false)]
+        [DataRow("2-9 c: ccccccccc", true)]
+        [DataRow("12-15 h: hhhhhrhcxhhhhhhhhb", true)]
+        public void ValidatePasswords(string line, bool expected)
+        {
+            Assert.AreEqual(expected, _day02.IsValid(line));
+        }
+
+        [TestMethod]
+        public void Part1()
+        {
+            var result = _day02.CountValidPasswords(_input);
+            Assert.AreEqual(454, result);
+        }
+
+        [TestMethod]
+        public void Part2Sample()
+        {
+            
+        }
+
+        [TestMethod]
+        public void Part2()
+        {
+            
+        }
+    }
+
+
+}
