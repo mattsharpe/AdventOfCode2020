7c1f089b23f38b8a38c50cf057cb700842692a6c
Matthew Sharpe <matthew.sharpe@sword-grc.com>
2020-12-07 17:56:53 +0000

    Sorting through luggage to find the total number of bags (using recursion which always makes my head hurt)

---
+++ Advent2020/Solutions/Day07.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Advent2020.Solutions
+{
+    class Day07
+    {
+
+        public int CountBagColoursReversed(string[] sample)
+        {
+            var bags = Parse(sample).ToList();
+            
+            //turn into a lookup of bag to 
+            var parents = bags.SelectMany(kvp => kvp.bags, (kvp, c) => (child: kvp.bag, parent: c.bag))
+                .ToLookup(x=>x.parent, x=>x.child);
+
+            var path = new HashSet<string>();
+
+            void FindParents(string key)
+            {
+                path.Add(key);
+                foreach (var parent in parents[key])
+                {
+                    FindParents(parent);
+                }
+            }
+
+            FindParents("shiny gold bag");
+            
+            //path will include the shiny gold bag as well.
+            return path.Count -1;
+        }
+
+        
+        public IEnumerable<(string bag, IEnumerable<(int count, string bag)> bags)> Parse(string[] input)
+        {
+            foreach (var line in input)
+            {
+                var bag = $"{line.Split("bag", StringSplitOptions.TrimEntries)[0]} bag";
+
+                var contents = Regex.Matches(line, @"(\d+) ([a-z]+ [a-z]+ bag)");
+                var bags = contents.Select(x => (Convert.ToInt32(x.Groups[1].Value), x.Groups[2].Value));
+                
+                yield return (bag, bags);
+            }
+        }
+
+
+        public int CountOfBagsNeeded(string[] input)
+        {
+            var bags = Parse(input).ToDictionary(x => x.bag, x => x.bags);
+
+            int Total(string bag)
+            {
+                var result = 1;
+                foreach (var (count, innerBag) in bags[bag])
+                {
+                    result += count * Total(innerBag);
+                    Console.WriteLine(result);
+                }
+
+                return result;
+            }
+
+            return Total("shiny gold bag") - 1;
+        }
+    }
+}
---
+++ Advent2020/Tests/Day07Tests.cs
@@ -0,0 +1,71 @@
+﻿using System.IO;
+using Advent2020.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2020.Tests
+{
+    [TestClass]
+    public class Day07Tests
+    {
+        private Day07 _day07;
+
+        private string[] Input => File.ReadAllLines("Input/Day07.txt");
+
+        private string[] _sample =
+        {
+            "light red bags contain 1 bright white bag, 2 muted yellow bags.",
+            "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+            "bright white bags contain 1 shiny gold bag.",
+            "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+            "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
+            "dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
+            "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
+            "faded blue bags contain no other bags.", "dotted black bags contain no other bags."
+        };
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _day07 = new Day07();
+        }
+
+        [TestMethod]
+        public void CountBagColoursReverseTree()
+        {
+            var result = _day07.CountBagColoursReversed(_sample);
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void Part1()
+        {
+            var result = _day07.CountBagColoursReversed(Input);
+            Assert.AreEqual(372, result);
+        }
+
+        [TestMethod]
+        public void BagCountSample()
+        {
+            var input = new[]
+            {
+                "shiny gold bags contain 2 dark red bags.",
+                "dark red bags contain 2 dark orange bags.",
+                "dark orange bags contain 2 dark yellow bags.",
+                "dark yellow bags contain 2 dark green bags.",
+                "dark green bags contain 2 dark blue bags.",
+                "dark blue bags contain 2 dark violet bags.",
+                "dark violet bags contain no other bags."
+            };
+
+            var result = _day07.CountOfBagsNeeded(input);
+            Assert.AreEqual(126, result);
+        }
+
+        [TestMethod]
+        public void CountOfAllBags()
+        {
+            var result = _day07.CountOfBagsNeeded(Input);
+            Assert.AreEqual(8015, result);
+        }
+    }
+}
