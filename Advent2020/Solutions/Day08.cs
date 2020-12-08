926e97c86e3f42a250e32cafbe1c14280ae91cd6
Matthew Sharpe <matthew.sharpe@sword-grc.com>
2020-12-08 14:23:01 +0000

    First vm problem of the year - fixing a bug in instructions to solve an infinite loop

---
+++ Advent2020/Solutions/Day08.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Advent2020.Solutions
+{
+    class Day08
+    {
+        public int Accumulator { get; set; }
+        
+        public bool RunProgramUntilLoopStarts(string[] input)
+        {
+            var processed = new HashSet<int>();
+
+            for (var i = 0; i < input.Length;)
+            {
+                if (processed.Contains(i))
+                {
+                    return false;
+                }
+                processed.Add(i);
+
+                i += ProcessInstruction(input[i]);
+            }
+
+            return true;
+        }
+
+        public int ProcessInstruction(string command)
+        {
+            var split = command.Split(" ");
+            var (instruction, value) = (split[0], Convert.ToInt32(split[1]));
+
+            switch (instruction)
+            {
+                case "nop":
+                    return 1;
+                case "jmp":
+                    return value;
+                case "acc":
+                    Accumulator += value;
+                    return 1;
+            }
+
+            return 0;
+        }
+
+        public int AlterProgramToCorrectLoop(string[] input)
+        {
+            var programs = GeneratePrograms(input);
+            
+            foreach (var program in programs)
+            {
+                Accumulator = 0;
+                var result = RunProgramUntilLoopStarts(program);
+                if (result)
+                {
+                    return Accumulator;
+                }
+            }
+
+            return -1;
+        }
+
+
+        //change one nop to jmp, or one jmp to nop
+        public IEnumerable<string[]> GeneratePrograms(string[] input)
+        {
+            for (var i = 0; i < input.Length; i++)
+            {
+                var instruction = input[i];
+                var result = (string[]) input.Clone();
+                
+                if (instruction.StartsWith("nop"))
+                {
+                    result[i] = instruction.Replace("nop", "jmp");
+                    yield return result.ToArray();
+                }
+                else if (instruction.StartsWith("jmp"))
+                {
+                    result[i] = instruction.Replace("jmp", "nop");
+                    yield return result.ToArray();
+                }
+            }
+        }
+    }
+}
---
+++ Advent2020/Tests/Day08Tests.cs
@@ -0,0 +1,76 @@
+﻿using System.IO;
+using Advent2020.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2020.Tests
+{
+    [TestClass]
+    public class Day08Tests
+    {
+        private Day08 _day08;
+        private string[] Input => File.ReadAllLines("Input/Day08.txt");
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _day08 = new Day08();
+        }
+
+        [TestMethod]
+        public void Sample()
+        {
+            var input = new[]
+            {
+                "nop +0",
+                "acc +1",
+                "jmp +4",
+                "acc +3",
+                "jmp -3",
+                "acc -99",
+                "acc +1",
+                "jmp -4",
+                "acc +6"
+            };
+
+            var completed = _day08.RunProgramUntilLoopStarts(input);
+            Assert.IsFalse(completed);
+            Assert.AreEqual(5, _day08.Accumulator);
+        }
+
+        [TestMethod]
+        public void Part1()
+        {
+            var completed = _day08.RunProgramUntilLoopStarts(Input);
+            Assert.IsFalse(completed);
+            Assert.AreEqual(1594, _day08.Accumulator);
+        }
+
+        [TestMethod]
+        public void AlterInstructionsSample()
+        {
+            var input = new[]
+            {
+                "nop +0 ",
+                "acc +1 ",
+                "jmp +4 ",
+                "acc +3 ",
+                "jmp -3 ",
+                "acc -99",
+                "acc +1 ",
+                "jmp -4 ",
+                "acc +6 ",
+            };
+
+            var result = _day08.AlterProgramToCorrectLoop(input);
+            Assert.AreEqual(8, result);
+        }
+
+        
+        [TestMethod]
+        public void AlterInstructionsToFixProgram()
+        {
+            var result = _day08.AlterProgramToCorrectLoop(Input);
+            Assert.AreEqual(758, result);
+        }
+    }
+}
