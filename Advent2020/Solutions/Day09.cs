3e491194676faf3f42db134df2d9add0f5bf25bf
Matthew Sharpe <matthew.sharpe@sword-grc.com>
2020-12-09 12:18:27 +0000

    Lovely bit of Linq to find sequences in a string of numbers

---
+++ Advent2020/Solutions/Day09.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace Advent2020.Solutions
+{
+    class Day09
+    {
+        public long FirstNumberNotSumOfPreceding(string[] input, int preamble)
+        {
+            var numbers = input.Select(long.Parse).ToList();
+            for (var i = 0; i < numbers.Count-preamble; i++)
+            {
+                var targetNumber = numbers[i + preamble];
+                var range = numbers.Skip(i).Take(preamble).ToArray();
+                var added = range.SelectMany(x => range, (x, y) => x + y);
+
+                if (!added.Contains(targetNumber))
+                {
+                    return targetNumber;
+                }
+            }
+
+            return 0;
+        }
+
+        public long SumOfRangeThatSumsToMismatchedNumber(string[] input, int preamble)
+        {
+            var numbers = input.Select(long.Parse).ToList();
+            var targetNumber = FirstNumberNotSumOfPreceding(input, preamble);
+
+            for (int i = 0; i < input.Length - preamble; i++)
+            {
+                var sum = 0L;
+                var range = numbers.Skip(i).TakeWhile(x =>
+                {
+                    sum += x;
+                    return sum <= targetNumber;
+                }).ToList();
+
+                if (range.Sum() == targetNumber)
+                    return range.Min() + range.Max();
+            }
+            return 0;
+        }
+    }
+}
---
+++ Advent2020/Tests/Day09Tests.cs
@@ -0,0 +1,71 @@
+﻿using System.IO;
+using Advent2020.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2020.Tests
+{
+    [TestClass]
+    public class Day09Tests
+    {
+        private Day09 _day09 = new Day09();
+
+        private string[] Input => File.ReadAllLines("Input/Day09.txt");
+        private readonly string[] _sample = 
+        {
+            "35",
+            "20",
+            "15",
+            "25",
+            "47",
+            "40",
+            "62",
+            "55",
+            "65",
+            "95",
+            "102",
+            "117",
+            "150",
+            "182",
+            "127",
+            "219",
+            "299",
+            "277",
+            "309",
+            "576"
+        };
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _day09 = new Day09();
+        }
+
+        [TestMethod]
+        public void Sample()
+        {
+            var result = _day09.FirstNumberNotSumOfPreceding(_sample, 5);
+            Assert.AreEqual(127, result);
+        }
+
+        [TestMethod]
+        public void FindFirstMismatchedNumber()
+        {
+            var result = _day09.FirstNumberNotSumOfPreceding(Input, 25);
+            Assert.AreEqual(69316178, result);
+        }
+
+        [TestMethod]
+        public void FindSumOfRangeThatAddsToMismatchedNumberSample()
+        {
+            var result = _day09.SumOfRangeThatSumsToMismatchedNumber(_sample, 5);
+            Assert.AreEqual(62, result);
+        }
+
+        [TestMethod]
+        public void FindSumOfRangeThatAddsToMismatchedNumber()
+        {
+            var result = _day09.SumOfRangeThatSumsToMismatchedNumber(Input, 25);
+            Assert.AreEqual(9351526, result);
+        }
+    }
+}
