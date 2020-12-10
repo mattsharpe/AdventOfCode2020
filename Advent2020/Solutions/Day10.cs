feab0fe583227eacc0cf0b94ffbb7114fb3ae022
Matthew Sharpe <matthew.sharpe@sword-grc.com>
2020-12-10 11:27:28 +0000

    Generating combinations of power adapters

---
+++ Advent2020/Solutions/Day10.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Advent2020.Solutions
+{
+    class Day10
+    {
+        public int ProductOfDifferences(string[] input)
+        {
+            //wrap the adapter collection with 0 (the terminal) and +3 for the device itself
+            var jolts = ParseToJolts(input);
+
+            var chain = jolts.Skip(1).Zip(jolts).Select(p => (a: p.First, b: p.Second)).ToList();
+
+            var ones = chain.Count(pair => pair.a - pair.b == 1);
+            var threes = chain.Count(pair => pair.a - pair.b == 3);
+
+            return ones * threes;
+        }
+
+        private List<int> ParseToJolts(string[] input)
+        {
+            var jolts = new List<int> {0};
+            jolts.AddRange(input.Select(int.Parse).OrderBy(x => x).ToList());
+            jolts.Add(jolts.Last() + 3);
+
+            return jolts;
+        }
+
+        public long NumberOfCombinations(string[] input)
+        {
+            var jolts = ParseToJolts(input);
+            //populate the cache with things we know, laptop is the end device
+            var cache = new Dictionary<int, long>
+            {
+                [jolts.Count - 1] = 0, 
+                [jolts.Count - 2] = 1
+            };
+
+            long BuildCache(int adapterIndex)
+            {
+                //memoization is key to performance here
+                if (cache.ContainsKey(adapterIndex))
+                    return cache[adapterIndex];
+
+                //work out all following combinations from here
+                var value = new[] {1, 2, 3}.Where(x =>
+                        adapterIndex + x < jolts.Count && jolts[adapterIndex + x] - jolts[adapterIndex] <= 3)
+                    .Sum(x => BuildCache(adapterIndex + x));
+
+                cache[adapterIndex] = value;
+                return value;
+            }
+
+            BuildCache(0);
+            return cache[0];
+        }
+    }
+}
---
+++ Advent2020/Tests/Day10Tests.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using Advent2020.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2020.Tests
+{
+    [TestClass]
+    public class Day10Tests
+    {
+        private Day10 _day10;
+        private string[] Input => File.ReadAllLines("Input/Day10.txt");
+        private readonly string[] _shortSample = 
+        {
+               "16", "10", "15", "5", "1", "11", "7", "19", "6", "12", "4"
+        };
+
+        private readonly string[] _longSample =
+        {
+            "28", "33", "18", "42", "31", "14", "46", "20", "48", "47", "24", "23", "49", "45", "19", 
+            "38", "39", "11", "1", "32", "25", "35", "8", "17", "7", "9", "4", "2", "34", "10", "3",
+        };
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _day10 = new Day10();
+        }
+
+        [TestMethod]
+        public void ProductOf1JoltAnd3JoltDifferencesSample()
+        {
+            var result  = _day10.ProductOfDifferences(_longSample);
+            Assert.AreEqual(220, result);
+        }
+
+        [TestMethod]
+        public void ProductOf1JoltAnd3JoltDifferences()
+        {
+            var result  = _day10.ProductOfDifferences(Input);
+            Assert.AreEqual(2210, result);
+        }
+
+        [TestMethod]
+        public void NumberOfCombinationsShortSample()
+        {
+            var result = _day10.NumberOfCombinations(_shortSample);
+            Assert.AreEqual(8, result);
+        }
+
+        [TestMethod]
+        public void NumberOfCombinationsLongSample()
+        {
+            var result = _day10.NumberOfCombinations(_longSample);
+            Assert.AreEqual(19208, result);
+        }
+
+        [TestMethod]
+        public void NumberOfCombinations()
+        {
+            var result = _day10.NumberOfCombinations(Input);
+            Assert.AreEqual(7086739046912, result);
+        }
+    }
+}
