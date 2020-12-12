ac167344dacc1f0b04b627a335b70bd4609c7806
mattsharpe <mattsharpe@users.noreply.github.com>
2020-12-12 09:26:15 +0000

    Feature/day11 (#3)
    * Day11
    
    * Part 1 done
    
    * Working out available seats on the ferry based on lines of sight and social tolerance
    
    Co-authored-by: Matt Sharpe <matthew.sharpe@sword-grc.com>

---
+++ Advent2020/Solutions/Day11.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Advent2020.Solutions
+{
+    class Day11
+    {
+        public int OccupiedSeats => _map.Cast<char>().Count(seat => seat == '#');
+
+        private char[,] _map;
+
+        public void Stabilise(string[] input, bool part2 = false)
+        {
+            ParseInput(input);
+            //PrintMap();
+            var previous = OccupiedSeats;
+
+            while (true)
+            {
+                if (part2)
+                {
+                    TakeTurn(CountVisibleOccupiedSeats, 5);
+                    //PrintMap();
+                }
+                else
+                {
+                    TakeTurn(CountAdjacentOccupiedSeats, 4);
+                }
+                
+                if (OccupiedSeats == previous)
+                    break;
+
+                previous = OccupiedSeats;
+            }
+        }
+
+        int CountAdjacentOccupiedSeats(int x, int y)
+        {
+            var offsets = new List<(int a, int b)>
+            {
+                (-1, -1), (-1, 0), (-1, 1),
+                (0, -1), (0, 1),
+                (1, -1), (1, 0), (1, 1)
+            };
+
+            var result = offsets.Count(offset =>
+            {
+                var newX = offset.a + x;
+                var newY = offset.b + y;
+                if (newX < _map.GetLowerBound(0) || newX > _map.GetUpperBound(0) ||
+                    newY < _map.GetLowerBound(1) || newY > _map.GetUpperBound(1)) return false;
+                return _map[newX, newY] == '#';
+            });
+
+            return result;
+        }
+
+        int CountVisibleOccupiedSeats(int x, int y)
+        {
+            var offsets = new List<(int x, int y)>
+            {
+                (-1, -1), (-1, 0), (-1, 1),
+                (0, -1), (0, 1),
+                (1, -1), (1, 0), (1, 1)
+            };
+
+            var result = offsets.Count(offset =>
+            {
+                var newX = offset.x + x;
+                var newY = offset.y + y;
+                if (newX < _map.GetLowerBound(0) || newX > _map.GetUpperBound(0) ||
+                    newY < _map.GetLowerBound(1) || newY > _map.GetUpperBound(1)) return false;
+
+                while (_map[newX, newY] == '.')
+                {
+                    newX += offset.x;
+                    newY += offset.y;
+
+                    if (newX < _map.GetLowerBound(0) || newX > _map.GetUpperBound(0) ||
+                        newY < _map.GetLowerBound(1) || newY > _map.GetUpperBound(1)) 
+                        return false;
+                }
+
+
+                return _map[newX, newY] == '#';
+            });
+
+            return result;
+        }
+
+        private void TakeTurn(Func<int, int, int> countOccupiedSeats, int tolerance)
+        {
+            var newSeats = new char[_map.GetUpperBound(0)+1, _map.GetUpperBound(1)+1];
+            for (var y = 0; y <= _map.GetUpperBound(1); y++)
+            {
+                for (var x = 0; x <= _map.GetUpperBound(0); x++)
+                {
+                    var current = _map[x, y];
+                    var occupiedSeats = countOccupiedSeats(x, y);
+                    switch (current)
+                    {
+                        case 'L' when occupiedSeats == 0:
+                            newSeats[x, y] = '#';
+                            continue;
+                        case '#' when occupiedSeats >= tolerance:
+                            newSeats[x, y] = 'L';
+                            continue;
+                        default:
+                            newSeats[x, y] = _map[x,y];
+                            break;
+                    }
+                }
+            }
+
+            _map = newSeats;
+        }
+
+        private void ParseInput(string[] input)
+        {
+            _map = new char[input[0].Length, input.Length];
+            for (var y = 0; y < input.Length; y++)
+            {
+                for (var x = 0; x < input[y].Length; x++)
+                {
+                    var square = input[y][x];
+                    _map[x, y] = square;
+
+                }
+            }
+        }
+
+
+        public void PrintMap()
+        {
+            Console.WriteLine($"-- {OccupiedSeats} --------------------------------");
+            for (var y = 0; y <= _map.GetUpperBound(1); y++)
+            {
+                for (var x = 0; x <= _map.GetUpperBound(0); x++)
+                {
+                    Console.Write(_map[x, y]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Advent2020/Tests/Day11Tests.cs
@@ -0,0 +1,74 @@
+﻿using System.IO;
+using Advent2020.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2020.Tests
+{
+    [TestClass]
+    public class Day11Tests
+    {
+        private Day11 _day11;
+
+        private string[] Input => File.ReadAllLines("Input/Day11.txt");
+        private readonly string[] _shortSample =
+        {
+            "L.LL.LL.LL",
+            "LLLLLLL.LL",
+            "L.L.L..L..",
+            "LLLL.LL.LL",
+            "L.LL.LL.LL",
+            "L.LLLLL.LL",
+            "..L.L.....",
+            "LLLLLLLLLL",
+            "L.LLLLLL.L",
+            "L.LLLLL.LL"
+        };
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _day11 = new Day11();
+        }
+
+        [TestMethod]
+        public void TakenSeatsWithSample()
+        {
+            _day11.Stabilise(_shortSample);
+
+            var result = _day11.OccupiedSeats;
+
+            Assert.AreEqual(37, result);
+        }
+
+        
+        [TestMethod]
+        public void TakenSeats()
+        {
+            _day11.Stabilise(Input);
+
+            var result = _day11.OccupiedSeats;
+
+            Assert.AreEqual(2265, result);
+        }
+
+        [TestMethod]
+        public void VisibleSeatsSample()
+        {
+            _day11.Stabilise(_shortSample, true);
+
+            var result = _day11.OccupiedSeats;
+
+            Assert.AreEqual(26, result);
+        }
+
+        [TestMethod]
+        public void VisibleSeats()
+        {
+            _day11.Stabilise(Input, true);
+
+            var result = _day11.OccupiedSeats;
+
+            Assert.AreEqual(2045, result);
+        }
+    }
+}
