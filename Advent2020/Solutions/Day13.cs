72c8e60e3cd44038e836275ea11958e708bba066
Matthew Sharpe <matthew.sharpe@sword-grc.com>
2020-12-13 14:29:44 +0000

    Finding the departure times of buses based on the modulus of departure time

---
+++ Advent2020/Solutions/Day13.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Advent2020.Solutions
+{
+    class Day13
+    {
+        public int FindFirstBus(string[] input)
+        {
+            var startTime = int.Parse(input[0]);
+            var buses = input[1].Split(",").Where(x => x != "x").Select(int.Parse).ToList();
+
+            Console.WriteLine(buses.Count);
+
+            bool found = false;
+            var currentTime = startTime;
+            while (!found)
+            {
+                currentTime++;
+                found = buses.Any(x => currentTime % x == 0);
+            }
+
+            var bus = buses.Single(x => currentTime % x == 0);
+            return  bus * (currentTime - startTime);
+
+        }
+    }
+}
---
+++ Advent2020/Tests/Day13Tests.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using Advent2020.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2020.Tests
+{
+    [TestClass]
+    public class Day13Tests
+    {
+        private Day13 _day13;
+        private string[] Input => File.ReadAllLines("Input/Day13.txt");
+        private readonly string[] _sample =
+        {
+            "939",
+            "7,13,x,x,59,x,31,19"
+        };
+
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _day13 = new Day13();
+        }
+
+        [TestMethod]
+        public void FirstBusSample()
+        {
+            var result = _day13.FindFirstBus(_sample);
+            Assert.AreEqual(295, result);
+        }
+
+        [TestMethod]
+        public void FindFirstBus()
+        {
+            var result = _day13.FindFirstBus(Input);
+            Assert.AreEqual(3789, result);
+        }
+    }
+}
