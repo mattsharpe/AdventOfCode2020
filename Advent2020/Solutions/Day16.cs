d8ea4c979275231a91b4caf1467842b81481b989
Matt Sharpe <matthew.sharpe@sword-grc.com>
2020-12-16 14:47:58 +0000

    Counting Invadid tickets based on allowed ranges.

---
+++ Advent2020/Solutions/Day16.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Advent2020.Solutions
+{
+    public class Day16
+    {
+        public int CountInvalidTickets(string input)
+        {
+            var regex = new Regex(@"(\d+)-(\d+)");
+            var split = input.Split($"{Environment.NewLine}{Environment.NewLine}");
+
+            var ranges = split[0].Split(Environment.NewLine).SelectMany(line =>
+            {
+                return regex.Matches(line).Select(x =>
+                    (Lower: int.Parse(x.Groups[1].Value), Upper: int.Parse(x.Groups[2].Value)));
+            }).ToList();
+
+            var ticketNumbers = split[2].Split(':')[1].Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+                .SelectMany(line => line.Split(',').Select(int.Parse))
+                .ToList();
+
+            return ticketNumbers.Where(number => !ranges.Any(range => range.Lower <= number && number <= range.Upper)).Sum();
+        }
+    }
+}
---
+++ Advent2020/Tests/Day16Tests.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using Advent2020.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2020.Tests
+{
+    [TestClass]
+    public class Day16Tests
+    {
+        private Day16 _day16;
+
+        private readonly string _sample =
+@"class: 1-3 or 5-7
+row: 6-11 or 33-44
+seat: 13-40 or 45-50
+
+your ticket:
+7,1,14
+
+nearby tickets:
+7,3,47
+40,4,50
+55,2,20
+38,6,12";
+
+        private readonly string Input = File.ReadAllText("Input/Day16.txt");
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _day16 = new Day16();
+        }
+
+        [TestMethod]
+        public void SumOfInvalidSampleTicketNumbers()
+        {
+            var result = _day16.CountInvalidTickets(_sample);
+            Assert.AreEqual(71, result);
+        }
+
+        [TestMethod]
+        public void SumOfInvalidTicketNumbers()
+        {
+            var result = _day16.CountInvalidTickets(Input);
+            Assert.AreEqual(24110, result);
+        }
+
+    }
+}
