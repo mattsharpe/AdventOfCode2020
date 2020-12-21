6e4c6052831b71cf0d61731e405d56d86d8a1cd2
Matt Sharpe <matthew.sharpe@sword-grc.com>
2020-12-21 19:56:55 +0000

    Using regexes and recursion to process an equation using different operator precedence

---
+++ Advent2020/Solutions/Day18.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Advent2020.Solutions
+{
+    class Day18
+    {
+
+        public long EvaluateEquation(string equation, bool additionHasPriority = false)
+        {
+            var solver = additionHasPriority ? (Func<string, long>) AdditionPriority : SameOperatorPrecedence;
+
+            while (true)
+            {
+                var brackets = new Regex(@"\(([\d+* ]*)\)");
+                if (!brackets.IsMatch(equation)) return solver(equation);
+
+                foreach (Match match in brackets.Matches(equation))
+                {
+                    var subPart = EvaluateEquation(match.Groups[1].Value, additionHasPriority);
+                    equation = equation.Replace(match.Value, subPart.ToString());
+                }
+            }
+        }
+
+        public long SumAllEquations(string[] input, bool additionHasPrecedence = false)
+        {
+            return input.Select(x=> EvaluateEquation(x, additionHasPrecedence)).Sum();
+        }
+
+        private long AdditionPriority(string input)
+        {
+            //process all additions then multiply to results
+            while (input.Contains("+"))
+            {
+                var sum = Regex.Match(input, @"(\d+) \+ (\d+)");
+                var a = Convert.ToInt64(sum.Groups[1].Value);
+                var b = Convert.ToInt64(sum.Groups[2].Value);
+                input = new Regex(@"\d+ \+ \d+").Replace(input, $"{a + b}", 1);
+            }
+            return input.Split("*")
+                .Select(long.Parse)
+                .Aggregate(1L, (a, b) => a * b);
+        }
+
+        private long SameOperatorPrecedence(string input)
+        {
+            var split = input.Split(' ');
+            var result = long.Parse(split[0]);
+            var operation = "+";
+
+            foreach (var instruction in split.Skip(1))
+            {
+                if (instruction == "+" || instruction == "*")
+                {
+                    operation = instruction;
+                }
+                else
+                {
+                    var value = long.Parse(instruction);
+                    result = operation == "+" ? result + value : result * value;
+                }
+            }
+            return result;
+        }
+
+        
+    }
+}
---
+++ Advent2020/Tests/Day18Tests.cs
@@ -0,0 +1,74 @@
+﻿using System.IO;
+using Advent2020.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2020.Tests
+{
+    [TestClass]
+    public class Day18Tests
+    {
+        private Day18 _day18;
+        
+        public string[] Input => File.ReadAllLines("Input/Day18.txt");
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _day18 = new Day18();
+        }
+
+        [TestMethod]
+        public void BasicSample()
+        {
+            var equation = "1 + 2 * 3 + 4 * 5 + 6";
+            Assert.AreEqual(71, _day18.EvaluateEquation(equation));
+        }
+
+        [TestMethod]
+        public void BasicSampleWithBrackets()
+        {
+            var equation = "1 + (2 * 3) + (4 * (5 + 6))";
+            Assert.AreEqual(51, _day18.EvaluateEquation(equation));
+        }
+
+        [DataTestMethod]
+        [DataRow("2 * 3 + (4 * 5)", 26)]
+        [DataRow("5 + (8 * 3 + 9 + 3 * 4 * 3)", 437)]
+        [DataRow("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 12240)]
+        [DataRow("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 13632)]
+        public void Samples(string equation, int expected)
+        {
+            Assert.AreEqual(expected, _day18.EvaluateEquation(equation));
+        }
+
+        [TestMethod]
+        public void SumOfAllEquations()
+        {
+            Assert.AreEqual(11297104473091, _day18.SumAllEquations(Input));
+        }
+
+        [TestMethod]
+        public void SampleAdditionPriority()
+        {
+            var equation = "1 + 2 * 3 + 4 * 5 + 6";
+            Assert.AreEqual(231, _day18.EvaluateEquation(equation, true));
+        }
+
+        [DataTestMethod]
+        [DataRow("1 + (2 * 3) + (4 * (5 + 6))", 51)]
+        [DataRow("2 * 3 + (4 * 5)", 46)]
+        [DataRow("5 + (8 * 3 + 9 + 3 * 4 * 3)", 1445)]
+        [DataRow("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 669060)]
+        [DataRow("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 23340)]
+        public void SamplesWithAdditionPriority(string equation, int expected)
+        {
+            Assert.AreEqual(expected, _day18.EvaluateEquation(equation, true));
+        }
+
+        [TestMethod]
+        public void SumOfAllEquationsWithAddition()
+        {
+            Assert.AreEqual(185348874183674, _day18.SumAllEquations(Input, true));
+        }
+    }
+}
