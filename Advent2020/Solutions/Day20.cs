11af2ba53d2cc1185d5cc52388b95760b9abf173
Matt Sharpe <matt@mattsharpe.co.uk>
2021-11-09 21:37:41 +0000

    Started work on day 20

---
+++ Advent2020/Solutions/Day20.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Advent2020.Solutions
+{
+    public class Day20
+    {
+        public Tile[] ParseInput(string input)
+        {
+            var tiles = input.Split($"{Environment.NewLine}{Environment.NewLine}").Select(x =>
+            {
+                var id = Regex.Match(x, "\\d+").Value;
+
+                return new Tile
+                {
+                    Id = int.Parse(id),
+                    Image = x.Split(Environment.NewLine).Skip(1).ToArray()
+                };
+            }).ToArray();
+
+            return tiles;
+
+        }
+
+        public Tile[][] MatchTiles(Tile[] tiles)
+        {
+            return null;
+        }
+
+        
+    }
+
+    public class Tile
+    {
+        public int Id { get; set; } 
+        public string[] Image { get; set; } 
+
+        public string Top { get { return Image.First(); } }
+        public string Bottom { get { return Image.Last(); } }
+
+        public string Left 
+        { 
+            get 
+            {
+                return string.Join("", Image.Select(x => x.First()));
+            } 
+        }
+
+        public string Right
+        {
+            get
+            {
+                return string.Join("", Image.Select(x => x.Last()));
+            }
+        }
+
+    }
+}
---
+++ Advent2020/Tests/Day20Tests.cs
@@ -0,0 +1,156 @@
+﻿using Advent2020.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace Advent2020.Tests
+{
+    [TestClass]
+    public class Day20Tests
+    {
+        private Day20 _day20;
+
+        private string _sample =
+@"Tile 2311:
+..##.#..#.
+##..#.....
+#...##..#.
+####.#...#
+##.##.###.
+##...#.###
+.#.#.#..##
+..#....#..
+###...#.#.
+..###..###
+
+Tile 1951:
+#.##...##.
+#.####...#
+.....#..##
+#...######
+.##.#....#
+.###.#####
+###.##.##.
+.###....#.
+..#.#..#.#
+#...##.#..
+
+Tile 1171:
+####...##.
+#..##.#..#
+##.#..#.#.
+.###.####.
+..###.####
+.##....##.
+.#...####.
+#.##.####.
+####..#...
+.....##...
+
+Tile 1427:
+###.##.#..
+.#..#.##..
+.#.##.#..#
+#.#.#.##.#
+....#...##
+...##..##.
+...#.#####
+.#.####.#.
+..#..###.#
+..##.#..#.
+
+Tile 1489:
+##.#.#....
+..##...#..
+.##..##...
+..#...#...
+#####...#.
+#..#.#.#.#
+...#.#.#..
+##.#...##.
+..##.##.##
+###.##.#..
+
+Tile 2473:
+#....####.
+#..#.##...
+#.##..#...
+######.#.#
+.#...#.#.#
+.#########
+.###.#..#.
+########.#
+##...##.#.
+..###.#.#.
+
+Tile 2971:
+..#.#....#
+#...###...
+#.#.###...
+##.##..#..
+.#####..##
+.#..####.#
+#..#.#..#.
+..####.###
+..#.#.###.
+...#.#.#.#
+
+Tile 2729:
+...#.#.#.#
+####.#....
+..#.#.....
+....#..#.#
+.##..##.#.
+.#.####...
+####.#.#..
+##.####...
+##..#.##..
+#.##...##.
+
+Tile 3079:
+#.#.#####.
+.#..######
+..#.......
+######....
+####.#..#.
+.#...#.##.
+#.#####.##
+..#.###...
+..#.......
+..#.###...";
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _day20 = new Day20();
+        }
+
+        [TestMethod]
+        public void ParseInput()
+        {
+            var tiles = _day20.ParseInput(_sample);
+            Assert.IsNotNull(tiles);
+            Assert.AreEqual(9, tiles.Length);
+        }
+
+        [TestMethod]
+        public void TileMethods()
+        {
+            var tile = _day20.ParseInput(_sample).First();
+
+            Assert.AreEqual(2311, tile.Id);
+            
+            Assert.AreEqual("..##.#..#.", tile.Top);
+            Assert.AreEqual("..###..###", tile.Bottom);
+            Assert.AreEqual(".#####..#.", tile.Left);
+            Assert.AreEqual("...#.##..#", tile.Right);
+        }
+
+        [TestMethod]
+        public void Part1()
+        {
+            _day20.ParseInput(File.ReadAllText("Input/Day20.txt"));
+        }
+    }
+}
