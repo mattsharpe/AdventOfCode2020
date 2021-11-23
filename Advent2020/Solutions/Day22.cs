7bd83e7781457ed1078913067ed6a72fbd75ec59
Matthew Sharpe <matthew.sharpe@sword-grc.com>
2021-11-23 13:44:19 +0000

    day22

---
+++ Advent2020/Solutions/Day22.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Advent2020.Solutions
+{
+    public class Day22
+    {
+        public int CalculateWinningScore(string[] input)
+        {
+            
+        }
+    }
+
+}
---
+++ Advent2020/Tests/Day22Tests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Advent2020.Solutions;
+
+namespace Advent2020.Tests
+{
+    [TestClass]
+    public class Day22Tests
+    {
+        private Day22 _day22;
+
+        private string[] _sample = new string[]
+        {
+            "Player 1:",
+            "9",
+            "2",
+            "6",
+            "3",
+            "1",
+            "",
+            "Player 2:",
+            "5",
+            "8",
+            "4",
+            "7",
+            "10",
+        };
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _day22 = new Day22();
+        }
+
+        [TestMethod]
+        public void Part1_Sample()
+        {
+            var result = _day22.CalculateWinningScore(_sample);
+
+            Assert.AreEqual(306, result);
+
+        }
+    }
+}
