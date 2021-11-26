1b3e09add02a4850e6143e1219a88c6d0a668998
mattsharpe <mattsharpe@users.noreply.github.com>
2021-11-26 09:27:33 +0000

    Playing a game of War against a 🦀 with a deck of cards  (#7)
    Along with a recursive sub game to keep things interesting (and slow)

---
+++ Advent2020/Solutions/Day22.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Advent2020.Solutions
+{
+    public class Day22
+    {
+        public int CalculateWinningScore(string[] input)
+        {
+            var game = ParseInput(input);
+
+            while (!game.Complete)
+            {
+                game.PlayRound();
+            }
+
+            game.Print();
+
+            return CalculateScore(game.Winner);
+        }
+
+        public int CalculateScore(Queue<int> deck)
+        {
+            return Enumerable.Range(1, deck.Count).Reverse().Zip(deck, (a, b) => a * b).Sum();   
+        }
+
+        private Game ParseInput(string[] input)
+        {
+            var player1 = input.TakeWhile(x => x != "");
+
+            var player2 = input.Skip(player1.Count() + 1);
+
+            return new Game
+            {
+                Deck1 = new Queue<int>(player1.Skip(1).Select(int.Parse)),
+                Deck2 = new Queue<int>(player2.Skip(1).Select(int.Parse))
+            };
+        }
+
+        public int CalculateWinningScoreOfRecursiveGame(string[] input)
+        {
+            var game = ParseInput(input);
+
+            //play game
+            PlayerOneWinsRecursiveRound(game);
+
+            return CalculateScore(game.Winner);
+        }
+
+        public bool PlayerOneWinsRecursiveRound(Game game)
+        {
+            var knownStates = new HashSet<string>();
+
+            while (!game.Complete)
+            {
+                //game.Print();
+                var state = string.Join(",", game.Deck1) + "/" + string.Join(",", game.Deck2);
+                if (knownStates.Contains(state))
+                {
+                    return true; //player one wins
+                }
+
+
+                knownStates.Add(state);
+
+                var one = game.Deck1.Dequeue();
+                var two = game.Deck2.Dequeue();
+
+                //Console.WriteLine($"Player 1 plays: {one}");
+                //Console.WriteLine($"Player 2 plays: {two}");
+                //Console.WriteLine();
+
+                var oneCanPlayOn = one <= game.Deck1.Count;
+                var twoCanPlayOn = two <= game.Deck2.Count;
+
+                bool playerOneWins;
+
+                if (!oneCanPlayOn || !twoCanPlayOn)
+                {
+                    playerOneWins = one > two;
+                }
+                else
+                {
+                    var recursiveGame = new Game
+                    {
+                        Deck1 = new Queue<int>(game.Deck1.Take(one)),
+                        Deck2 = new Queue<int>(game.Deck2.Take(two)),
+                    };
+                    playerOneWins = PlayerOneWinsRecursiveRound(recursiveGame);
+                }
+
+                //winners card, then losers, regardless of number
+                if (playerOneWins)
+                {
+                    game.Deck1.Enqueue(one);
+                    game.Deck1.Enqueue(two);
+                }
+                else
+                {
+                    game.Deck2.Enqueue(two);
+                    game.Deck2.Enqueue(one);
+                }
+            }
+
+            return game.Deck1.Count > 0;
+        }
+    }
+
+    public record Game
+    {
+        public Queue<int> Deck1 { get; set; }
+        public Queue<int> Deck2 { get; set; }
+
+        public bool Complete => Deck1.Count == 0 || Deck2.Count == 0;
+
+        public Queue<int> Winner => Deck1.Count > 0 ? Deck1 : Deck2;
+
+        public void Print()
+        {
+            Console.WriteLine($"Player 1's deck: {string.Join(",", Deck1)}");
+            Console.WriteLine($"Player 2's deck: {string.Join(",", Deck2)}");
+        }
+
+        public void PlayRound()
+        {
+            var one = Deck1.Dequeue();
+            var two = Deck2.Dequeue();
+
+            if (one > two)
+            {
+                Deck1.Enqueue(one);
+                Deck1.Enqueue(two);
+            }
+            else
+            {
+                Deck2.Enqueue(two);
+                Deck2.Enqueue(one);
+            }
+        }
+        
+    }
+
+}
---
+++ Advent2020/Tests/Day22Tests.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Advent2020.Solutions;
+using System.IO;
+
+namespace Advent2020.Tests
+{
+    [TestClass]
+    public class Day22Tests
+    {
+        private Day22 _day22;
+
+        private string[] _sample = new string[]
+        {
+            "Player 1:",
+            "9",
+            "2",
+            "6",
+            "3",
+            "1",
+            "",
+            "Player 2:",
+            "5",
+            "8",
+            "4",
+            "7",
+            "10",
+        };
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _day22 = new Day22();
+        }
+
+        [TestMethod]
+        public void Part1_Sample()
+        {
+            var result = _day22.CalculateWinningScore(_sample);
+
+            Assert.AreEqual(306, result); 
+        }
+
+        [TestMethod]
+        public void Part1()
+        {
+            var result = _day22.CalculateWinningScore(File.ReadAllLines("Input/Day22.txt"));
+
+            Assert.AreEqual(35818, result);
+        }
+
+        [TestMethod]
+        public void LoopExits()
+        {
+            var input = new[]
+            {
+                "Player 1:",
+                "43",
+                "19",
+                "",
+                "Player 2:",
+                "2",
+                "29",
+                "14",
+            };
+
+            var result = _day22.CalculateWinningScoreOfRecursiveGame(input);
+
+            Assert.AreEqual(105, result);
+        }
+
+        [TestMethod]
+        public void RecursiveGameSample()
+        {
+            int result = _day22.CalculateWinningScoreOfRecursiveGame(_sample);
+
+            Assert.AreEqual(291, result);
+        }
+
+        [TestMethod]
+        public void RecursiveGame()
+        {
+            int result = _day22.CalculateWinningScoreOfRecursiveGame(File.ReadAllLines("Input/Day22.txt"));
+
+            Assert.AreEqual(34771, result);
+        }
+    }
+}
