59dfd4c1483f5460ad1da7c6e14ab1e0cb75fa75
Matt Sharpe <matt@mattsharpe.co.uk>
2022-11-29 22:36:33 +0000

    Day 25: Combo Breaker

---
+++ Advent2020/Solutions/Day25.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Linq;
+
+namespace Advent2020.Solutions
+{
+    public class Day25
+    {
+        public long Part1(string[] input)
+        {
+            var cardLoopSize = GetLoopSize(Convert.ToInt32(input.First()));
+
+            return GetKey(Convert.ToInt32(input.Last()), cardLoopSize);
+        }
+        
+        private int GetLoopSize(int publicKey)
+        {
+            var subjectNumber = 7;
+            var i = 0;
+            var value = 1;
+
+            while (value != publicKey)
+            {
+                value = (value * subjectNumber) % 20201227;
+                i++;
+            }
+            return i;
+        }
+
+        private long GetKey(int publicKey, int loopSize)
+        {
+            long value = 1;
+            for (int i = 0; i < loopSize; i++)
+            {
+                value = value * publicKey % 20201227;
+            }
+
+            return value;
+        }
+    }
+}
---
+++ Advent2020/Tests/Day25Tests.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using Advent2020.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Advent2020.Tests
+{
+    [TestClass]
+    public class Day25Tests
+    {
+        private Day25 _day25 = new();
+        string[] Input => File.ReadAllLines(@"Input\Day25.txt");
+
+        private string[] Sample = { "5764801", "17807724" };
+
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _day25 = new Day25();
+        }
+
+        [TestMethod]
+        public void Part1_Sample()
+        {
+            var result = _day25.Part1(Sample);
+
+            Assert.AreEqual(14897079, result);
+        }
+
+        [TestMethod]
+        public void Part1()
+        {
+            var result = _day25.Part1(Input);
+
+            Assert.AreEqual(11707042, result);
+        }
+    }
+}
